e8606c2da24c0e04b5bb120842236317685f1d55
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-02 16:14:06 +0900

    feat (InputView) : 라운드마다 플레이어가 이동할 칸을 입력 받는 기능
    - 입력 받는 값이 "U" 또는 "D"가 아닌 경우, 예외를 던진다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,13 +3,20 @@ package bridge.view;
 import bridge.dto.GameCommand;
 import bridge.dto.Space;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final Map<String, Space> FORMAT_TO_SPACE = Map.of("U", Space.UP, "D", Space.DOWN);
+
     private static final String INPUT_BRIDGE_REQUEST = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_SPACE_REQUEST
+            = "이동할 칸을 선택해주세요. (위: %s, 아래: %s)".formatted(UP, DOWN);
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,7 +42,17 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public Space readMoving() {
-        return null;
+        println(INPUT_SPACE_REQUEST);
+        return readSpace();
+    }
+
+    private Space readSpace() {
+        String message = read().trim();
+
+        if (FORMAT_TO_SPACE.containsKey(message)) {
+            return FORMAT_TO_SPACE.get(message);
+        }
+        throw new IllegalArgumentException("이동할 칸은 %s 또는 %s만 입력해야 합니다".formatted(UP, DOWN));
     }
 
     /**
