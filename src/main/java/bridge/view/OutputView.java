66eadc92a074ea538b1f1f39a93c619293bb0aab
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-02 16:42:10 +0900

    feat (OutputVIew) : 다리 현재 상황 출력 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,11 +1,23 @@
 package bridge.view;
 
+import static java.util.stream.Collectors.joining;
+
 import bridge.dto.BridgeDto;
+import bridge.dto.Space;
+import java.util.stream.IntStream;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    private static final String BRIDGE_PREFIX = "[ ";
+    private static final String BRIDGE_DELIMITER = " | ";
+    private static final String BRIDGE_SUFFIX = " ]";
+    private static final String CORRECT = "O";
+    private static final String WRONG = "X";
+    private static final String NOTHING = " ";
+
 
     private static final String START_GAME = "다리 건너기 게임을 시작합니다.";
     private static final String ERROR_FORMAT = "[ERROR] %s%n";
@@ -18,6 +30,24 @@ public class OutputView {
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      */
     public void printMap(BridgeDto bridgeDto) {
+        println(makeBridgeLine(bridgeDto, Space.UP));
+        println(makeBridgeLine(bridgeDto, Space.DOWN));
+    }
+
+    private String makeBridgeLine(BridgeDto bridgeDto, Space space) {
+        return IntStream.range(0, bridgeDto.trace().size())
+                .mapToObj(index -> findBridgeBlock(bridgeDto, space, index))
+                .collect(joining(BRIDGE_DELIMITER, BRIDGE_PREFIX, BRIDGE_SUFFIX));
+    }
+
+    private String findBridgeBlock(BridgeDto bridgeDto, Space space, int index) {
+        if (bridgeDto.trace().get(index) != space) {
+            return NOTHING;
+        }
+        if (bridgeDto.bridge().get(index) == space) {
+            return CORRECT;
+        }
+        return WRONG;
     }
 
     /**
