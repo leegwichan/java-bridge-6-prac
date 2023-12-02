723c3beb80ec5e37006611ac21111a322ddfb597
LastCow9000 <sys19912002@hanmail.net>
2023-12-02 16:31:54 +0900

    feat: 게임 다시 시작하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,24 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final String RETRY = "R";
+
+    private final List<String> bridge;
+    private int round;
+    private UserInputs userInputs;
+    private int trialCount;
+
+    public BridgeGame(List<String> bridge, UserInputs userInputs) {
+        this.bridge = bridge;
+        this.round = 0;
+        this.trialCount = 1;
+        this.userInputs = userInputs;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -11,6 +26,7 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
+
     }
 
     /**
@@ -18,6 +34,21 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String input) {
+        if (input.equals(RETRY)) {
+            userInputs.reset();
+            increaseTrialCount();
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getTrialCount() {
+        return trialCount;
+    }
+
+    private void increaseTrialCount() {
+        trialCount++;
     }
 }
---
+++ src/main/java/bridge/UserInputs.java
@@ -0,0 +1,20 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInputs {
+    private List<String> userInputs;
+
+    private UserInputs(List<String> userInputs) {
+        this.userInputs = userInputs;
+    }
+
+    public static UserInputs from(List<String> input) {
+        return new UserInputs(input);
+    }
+
+    public void reset() {
+        this.userInputs = new ArrayList<>();
+    }
+}
