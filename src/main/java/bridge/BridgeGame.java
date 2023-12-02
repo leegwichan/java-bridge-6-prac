fd52ed344637d06cea671380b755f1c5ac54392a
Aaron(constant94-dev) <constant94@naver.com>
2023-12-02 17:58:26 +0900

    feat(bridge): 게임 출발지 & 실제 동작 로직 메서드 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,7 +1,10 @@
 package bridge;
 
+import static bridge.constant.Game.GAME_FAIL;
 import static bridge.constant.Room.ROOM_EMPTY;
 import static bridge.constant.Room.ROOM_EXIST;
+
+import bridge.model.Result;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -14,12 +17,14 @@ public class BridgeGame {
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public String move(String currentBridge, String move) {
-        // 건널 수 있다면 O
-        // 없다면 X 표시
-        if (currentBridge.equals(move)) {
+        // 반복이 아니라 현재 이동한 다리의 위치와 비교 해야 한다.
+        // 그러기 위해서는 플레이어가 얼마나 이동했는지 알아야한다.
+        // 이동 카운트를 하자!
+
+        if (currentBridge.equals(move)) { // 건널 수 있다면 O
             return ROOM_EXIST.getMessage();
         }
-        return ROOM_EMPTY.getMessage();
+        return ROOM_EMPTY.getMessage(); // 없다면 X 표시
     }
 
 
@@ -28,13 +33,16 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public int retry(int totalTry) {
+    public Result retry(Result gameResult) {
         // 재시도() 하면 retry 상수 반환
         // 종료() 하면 quit 상수 반환
         // 반환된 문자열은 어떤 출력을 보여줄지로 사용된다.
         // 재시작 할 때 필요한 것은
         // 총 시도한 횟수 1증가
         // 이전에 이동한 칸 초기화
-        return totalTry++;
+        int totalTry = gameResult.getTotalTry();
+        gameResult = new Result(GAME_FAIL.getMessage(), totalTry + 1);
+
+        return gameResult;
     }
 }
---
+++ src/main/java/bridge/BridgeGameController.java
@@ -0,0 +1,54 @@
+package bridge;
+
+import static bridge.constant.Game.GAME_FAIL;
+import static bridge.constant.Game.GAME_QUIT;
+import static bridge.constant.Room.ROOM_EMPTY;
+
+import bridge.model.Result;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.List;
+
+public class BridgeGameController {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private BridgeMaker bridgeMaker;
+    private BridgeGame bridgeGame;
+    private Result gameResult;
+
+    public void run() {
+        // TODO: 실제 동작 로직
+        ready();
+        bridgeGamePlay();
+    }
+
+    private void ready() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        bridgeGame = new BridgeGame();
+        gameResult = new Result(GAME_FAIL.getMessage(), 1);
+    }
+
+    private void bridgeGamePlay() {
+        outputView.printHelloBridgeGame();
+        int bridgeSize = inputView.readBridgeSize();
+        List<String> bridges = bridgeMaker.makeBridge(bridgeSize);
+
+        while (true) {
+            String moveResult = inputView.readMoving(bridges);// X 가 리턴되면 결과 출력 후 재시도 여부를 묻는다
+
+            if (moveResult.equals(ROOM_EMPTY.getMessage())) {
+                String gameCommand = inputView.readGameCommand();
+                if (gameCommand.equals(GAME_QUIT.getMessage())) {
+                    outputView.printResult(gameResult); // 최종 게임 결과 출력 후 프로그램 종료
+                    break;
+                }
+                gameResult = bridgeGame.retry(gameResult);// 게임을 다시 시도
+            }
+        }
+
+
+    }
+}
