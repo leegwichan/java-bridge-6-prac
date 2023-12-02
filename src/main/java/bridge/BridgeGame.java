f418a401b0c47a08946ae2bbe58f6c95d9e0bf9f
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-02 15:05:03 +0900

    feat (BridgeGame) : 재시도 시, 현재까지 건넌 길을 초기화하는 기능 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,9 +1,18 @@
 package bridge;
 
-/**
- * 다리 건너기 게임을 관리하는 클래스
- */
+import bridge.model.Bridge;
+import bridge.model.Trace;
+import java.util.Objects;
+
 public class BridgeGame {
+
+    private final Bridge bridge;
+    private Trace trace;
+
+    public BridgeGame(Bridge bridge, Trace trace) {
+        this.bridge = Objects.requireNonNull(bridge);
+        this.trace = Objects.requireNonNull(trace);
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -15,9 +24,8 @@ public class BridgeGame {
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        trace = new Trace(bridge.size());
     }
 }
