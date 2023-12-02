37cf8204c7193c6e9732627aaccd0e1a983c1954
Aaron(constant94-dev) <constant94@naver.com>
2023-12-02 16:22:44 +0900

    feat(bridge): 입력 UI 관련 메서드 및 테스트 코드 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,11 @@
 package bridge;
 
+import static bridge.exception.ExceptionHandler.invalidGameCommand;
+import static bridge.exception.ExceptionHandler.invalidMove;
+import static bridge.exception.ExceptionHandler.invalidNumber;
+
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,20 +15,48 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        String bridgeSize = Console.readLine();
+
+        validateBridgeSize(bridgeSize);
+
+        return Integer.parseInt(bridgeSize);
+    }
+
+    public void validateBridgeSize(String bridgeSize) {
+        if (!bridgeSize.matches("^(?:[3-9]|1\\d|20)$")) {
+            invalidNumber();
+        }
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        String move = Console.readLine();
+        validateMove(move);
+
+        return move;
+    }
+
+    public void validateMove(String move) {
+        if (!move.matches("[UD]")) {
+            invalidMove();
+        }
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameCommand = Console.readLine();
+        validateGameCommand(gameCommand);
+
+        return gameCommand;
+    }
+
+    public void validateGameCommand(String gameCommand) {
+        if (!gameCommand.matches("[RU]")) {
+            invalidGameCommand();
+        }
     }
 }
---
+++ src/test/java/bridge/InputViewTest.java
@@ -0,0 +1,48 @@
+package bridge;
+
+import static bridge.constant.Exception.EXCEPTION_INVALID_GAME_COMMAND;
+import static bridge.constant.Exception.EXCEPTION_INVALID_MOVE;
+import static bridge.constant.Exception.EXCEPTION_INVALID_NUMBER;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputViewTest {
+
+    private InputView inputView;
+
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("입력 받은 다리 길이가 3부터 20 사이의 숫자 아닐 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "21", "a", "bb", "pppp"})
+    void createBridgeSizeBetweenThreeToTwenty(String badgeSize) {
+        assertThatThrownBy(() -> inputView.validateBridgeSize(badgeSize))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(EXCEPTION_INVALID_NUMBER.getMessage());
+    }
+
+    @DisplayName("입력 받은 이동할 칸이 U(위 칸)와 D(아래 칸)가 아닐 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "a", "Z", "z", "123", "한글사랑"})
+    void createMoveToUOrD(String move) {
+        assertThatThrownBy(() -> inputView.validateMove(move))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(EXCEPTION_INVALID_MOVE.getMessage());
+    }
+
+    @DisplayName("입력 받은 게임 R(재시작)과 Q(종료)가 아닐 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"A", "a", "Z", "z", "123", "한글사랑"})
+    void createGameCommandToROrQ(String gameCommand) {
+        assertThatThrownBy(() -> inputView.validateGameCommand(gameCommand))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(EXCEPTION_INVALID_GAME_COMMAND.getMessage());
+    }
+}
