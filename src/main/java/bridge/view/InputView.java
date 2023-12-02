e61d76271c7ed8ee88d443accb299541a3a4d2c1
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-12-02 16:06:39 +0900

    feat (InputView) : 다리 길이를 입력받는 기능 구현
    - 숫자가 아닐 경우 예외를 던진다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -2,17 +2,33 @@ package bridge.view;
 
 import bridge.dto.GameCommand;
 import bridge.dto.Space;
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
 
+    private static final String INPUT_BRIDGE_REQUEST = "다리의 길이를 입력해주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        println(INPUT_BRIDGE_REQUEST);
+        return readInt();
+    }
+
+    private int readInt() {
+        try {
+            return tryParsingToInt(read());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자를 입력해야 합니다");
+        }
+    }
+
+    private int tryParsingToInt(String inputMessage) {
+        return Integer.parseInt(inputMessage);
     }
 
     /**
@@ -27,5 +43,13 @@ public class InputView {
      */
     public GameCommand readGameCommand() {
         return null;
+    }
+
+    private void println(String message) {
+        System.out.println(message);
+    }
+
+    private String read() {
+        return Console.readLine();
     }
 }
