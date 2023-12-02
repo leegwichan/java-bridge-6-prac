7b10300ff01bd04712f9f6dc314a8f0affc9d270
LastCow9000 <sys19912002@hanmail.net>
2023-12-02 17:05:20 +0900

    feat: 플레이어가 이동할 칸 입력받는 기능 구현

--- src/main/java/bridge/exception/ErrorMessage.java
+++ src/main/java/bridge/exception/ErrorMessage.java
@@ -2,6 +2,8 @@ package bridge.exception;
 
 public class ErrorMessage {
     public static final String WRONG_BRIDGE_SIZE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String WRONG_MOVE_INPUT = "U 또는 D의 값만 입력하여야 합니다.";
+
 
     private ErrorMessage() {
     }
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -9,6 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVE_POSITION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -27,7 +28,18 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        print(INPUT_MOVE_POSITION);
+        String input = read();
+        validateMoveInput(input);
+
+        return input;
+    }
+
+    private void validateMoveInput(String input) {
+        if (input.equals("U") || input.equals("D")) {
+            return;
+        }
+        throw new InputException(ErrorMessage.WRONG_MOVE_INPUT);
     }
 
     /**
