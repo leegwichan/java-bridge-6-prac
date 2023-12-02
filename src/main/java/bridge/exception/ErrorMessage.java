992dc564d65f98e75ed47d95c0bcf58641293924
LastCow9000 <sys19912002@hanmail.net>
2023-12-02 17:09:18 +0900

    feat: 게임 재시작 여부 입력받는 기능 구현

--- src/main/java/bridge/exception/ErrorMessage.java
+++ src/main/java/bridge/exception/ErrorMessage.java
@@ -3,6 +3,7 @@ package bridge.exception;
 public class ErrorMessage {
     public static final String WRONG_BRIDGE_SIZE = "다리 길이는 3부터 20 사이의 숫자여야 합니다.";
     public static final String WRONG_MOVE_INPUT = "U 또는 D의 값만 입력하여야 합니다.";
+    public static final String WRONG_COMMAND_INPUT = "R 또는 Q의 값만 입력하여야 합니다.";
 
 
     private ErrorMessage() {
--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -10,6 +10,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String INPUT_MOVE_POSITION = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String INPUT_RESET_OR_QUIT = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,18 +36,15 @@ public class InputView {
         return input;
     }
 
-    private void validateMoveInput(String input) {
-        if (input.equals("U") || input.equals("D")) {
-            return;
-        }
-        throw new InputException(ErrorMessage.WRONG_MOVE_INPUT);
-    }
-
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        print(INPUT_RESET_OR_QUIT);
+        String input = read();
+        validateGameCommand(input);
+
+        return input;
     }
 
     private String read() {
@@ -55,5 +53,19 @@ public class InputView {
 
     private void print(String message) {
         System.out.println(message);
+    }
+
+    private void validateMoveInput(String input) {
+        if (input.equals("U") || input.equals("D")) {
+            return;
+        }
+        throw new InputException(ErrorMessage.WRONG_MOVE_INPUT);
+    }
+
+    private void validateGameCommand(String input) {
+        if (input.equals("R") || input.equals("Q")) {
+            return;
+        }
+        throw new InputException(ErrorMessage.WRONG_COMMAND_INPUT);
     }
 }
